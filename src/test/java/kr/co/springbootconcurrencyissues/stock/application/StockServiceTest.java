6862c6566d9fa8d1cf9d5739e60a5b8200d8960a
HyuckJuneHong <jjj8337721@naver.com>
2023-10-06 20:48:53 +0900

    test: 100개의 요청이 동시에 들어왔을 때 테스트 코드
    레이스 컨디션이 발생하면 실패

--- src/test/java/kr/co/springbootconcurrencyissues/stock/application/StockServiceTest.java
+++ src/test/java/kr/co/springbootconcurrencyissues/stock/application/StockServiceTest.java
@@ -9,6 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -33,14 +37,39 @@ class StockServiceTest {
     @DisplayName("decrease - 1개의 요청에 대한 재고 감소가 성공적으로 이뤄지는 지 검증")
     @Test
     void decrease_1_request_quantity() {
-        // Given
-        stockService.decrease(1L, 1);
-
         // When
+        stockService.decrease(1L, 1);
         Stock actual = stockRepository.findById(1L)
                 .orElseThrow(IllegalArgumentException::new);
 
         // Then
         assertThat(actual.getQuantity()).isEqualTo(99);
+    }
+
+    @DisplayName("decrease - 100개의 요청에 대한 재고 감소가 성공적으로 이뤄지는 지 검증")
+    @Test
+    void decrease_100_request_quantity() throws InterruptedException {
+        // Given
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        // When
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+        Stock actual = stockRepository.findById(1L)
+                .orElseThrow(IllegalArgumentException::new);
+
+        // Then
+        assertThat(actual.getQuantity()).isZero();
     }
 }
