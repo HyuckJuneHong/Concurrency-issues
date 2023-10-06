7cdf720934ea86736ef5d61b4fb397e470649861
HyuckJuneHong <jjj8337721@naver.com>
2023-10-06 18:43:38 +0900

    init: 프로젝트 세팅

---
+++ README.md
@@ -0,0 +1,7 @@
+# 재고 시스템으로 알아보는 동시성 이슈
+
+### 개발 환경
+- JAVA 17
+- Springboot 3.1.4
+- Gradle
+- MySQL
---
+++ src/main/java/kr/co/springbootconcurrencyissues/SpringbootConcurrencyIssuesApplication.java
@@ -0,0 +1,13 @@
+package kr.co.springbootconcurrencyissues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConcurrencyIssuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConcurrencyIssuesApplication.class, args);
+    }
+
+}
