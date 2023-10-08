cbbbcafcc9817e4d21a19308dd0bd92ce02f4569
HyuckJuneHong <jjj8337721@naver.com>
2023-10-08 17:47:22 +0900

    feat: Redisson 기능 추가 (동시성 이슈 해결)

---
+++ src/main/java/kr/co/springbootconcurrencyissues/stock/application/facade/RedissonLockStockFacade.java
@@ -0,0 +1,41 @@
+package kr.co.springbootconcurrencyissues.stock.application.facade;
+
+import kr.co.springbootconcurrencyissues.stock.application.StockService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RedissonLockStockFacade {
+
+    private final RedissonClient redissonClient;
+    private final StockService stockService;
+
+    public void decrease(Long id, long quantity) {
+        // 락 객체를 가져온다.
+        RLock lock = redissonClient.getLock(id.toString());
+
+        try {
+            // 몇 초 동안 락 획득을 시도할 것인지, 그리고 몇 초 동안 락을 점유할 것인지 설정한다.
+            boolean available = lock.tryLock(10, 1, TimeUnit.SECONDS);
+
+            // 만약 락 획득을 실패한다면 로그를 남긴다.
+            if (!available) {
+                log.warn("락 획득 실패");
+                return;
+            }
+
+            stockService.decrease(id, quantity);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("error : " + e);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
